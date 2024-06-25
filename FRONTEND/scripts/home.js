1439f3a4a1d7de1bd214959ddd99daf5e77d6ecc
Queen Esther Onduku <qonduku@gmail.com>
2024-06-25 22:50:44 +0100

    product section

--- FRONTEND/scripts/home.js
+++ FRONTEND/scripts/home.js
@@ -7,3 +7,14 @@
         nav.classList.toggle('active');
     });
 });
+
+
+JavaScript code.   
+let images = document.querySelectorAll('#image-slider img');
+let currentImage = 0;
+
+setInterval(() => {
+  images[currentImage].style.display = 'none';
+  currentImage = (currentImage + 1) % images.length;
+  images[currentImage].style.display = 'block';
+}, 10000); // 3000ms = 3 seconds
