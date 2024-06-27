1475413e712eda38c5d3beebeedc0e4d274a1106
elijah571 <elijahfx43@gmail.com>
2024-06-27 22:34:55 +0100

    product header page updated

---
+++ FRONTEND/scripts/products.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector(".menu");
+const clearBtn = document.querySelector(".clear");
+const icons = document.querySelector(".icons-js");
+
+menuBtn.addEventListener("click", () => {
+    icons.style.display = 'block';
+   
+});
+clearBtn.addEventListener("click", () => {
+    icons.style.display = 'none';
+   
+});
