35e7059d4990598228806075de7084c70d343013
buyekeobare <buyekeobare@gmail.com>
2024-07-03 21:15:04 +0300

    added token

--- BACKEND/X_BETA/urls.py
+++ BACKEND/X_BETA/urls.py
@@ -11,4 +11,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
