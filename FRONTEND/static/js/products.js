c05a1a8dac9c3f7d631c1a8f2ee4bc31a9d34ebd
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-18 21:12:58 +0100

    product fixed

--- FRONTEND/static/js/products.js
+++ FRONTEND/static/js/products.js
@@ -11,68 +11,84 @@ clearBtn.addEventListener("click", () => {
     icons.style.display = 'none';
    
 });
+
 // product section
-const products = [{
-    id: "abc1",
-    image: "img/ace-img.jpg",
-    name: "Ace Elec 200000 MAh",
-    price: 8500,
-},{
-    id: "abc2",
-    image: "img/samsum-img.jpg",
-    name: "Samsung Galaxy A05",
-    price: 104999,
-},{
-    id: "abc3",
-    image: "img/oraimo-img.jpg",
-    name: "Oraimo TWS",
-    price: 13000,
-},{
-    id: "abcd4",
-    image:"img/hp-promax-img.jpg",
-    name: "Hp ProBook 11 X360",
-    price: 19300,
-},{
-    id: "abcd5",
-    image: "img/adoera-camera-img.jpg",
-    name: "Adoer 1080 D  Camera",
-    price: 116990,
-},{
-    id: "abcd6",
-    image: "img/mp4-player-img.jpg",
-    name: "MP3 MP4 Music Player",
-    price: 42000,
-},{
-    id: "abcd7",
-    image: "img/car-charger-img.jpg",
-    name: "G7 Bluetooth Car Charger",
-    price: 4900,
-},{
-    id: "abcd8",
-    image: "img/graphic-drawer-img.jpg",
-    name: "Graphics Drawing Table",
-    price: 36390,
-}];
-console.log(products);
-let productHTML ='';
-products.forEach((product) => {
-    productHTML += `
-        <div class="product">
-            <a href="product_details.html">
-                 <div class="product-img">
-                    <img src="${product.image}">
-                </div>
-                <p class="name">${product.name}</p>
-                <p class="price">${product.price}</p>
+document.addEventListener("DOMContentLoaded", function() {
+    const products = [{
+        id: "abc1",
+        image: productImages.ace,
+        name: "Ace Elec 200000 MAh",
+        price: 8500,
+    }, {
+        id: "abc2",
+        image: productImages.samsung,
+        name: "Samsung Galaxy A05",
+        price: 104999,
+    }, {
+        id: "abc3",
+        image: productImages.oraimo,
+        name: "Oraimo TWS",
+        price: 13000,
+    }, {
+        id: "abcd4",
+        image: productImages.hp,
+        name: "Hp ProBook 11 X360",
+        price: 19300,
+    }, {
+        id: "abcd5",
+        image: productImages.adoer,
+        name: "Adoer 1080 D  Camera",
+        price: 116990,
+    }, {
+        id: "abcd6",
+        image: productImages.mp4,
+        name: "MP3 MP4 Music Player",
+        price: 42000,
+    }, {
+        id: "abcd7",
+        image: productImages.carCharger,
+        name: "G7 Bluetooth Car Charger",
+        price: 4900,
+    }, {
+        id: "abcd8",
+        image: productImages.graphicDrawer,
+        name: "Graphics Drawing Table",
+        price: 36390,
+    }];
 
-                <div class="btn">
-                    <button class="add-to-cart-js">Add To Cart</button>
-                </div>
-            </a>
+    let productHTML = '';
+    products.forEach((product) => {
+        productHTML += `
+            <div class="product">
+                <a href="product_details.html">
+                    <div class="product-img">
+                        <img src="${product.image}">
+                    </div>
+                    <p class="name">${product.name}</p>
+                    <p class="price">₦${product.price}</p>
+
+                    <div class="btn">
+                        <button class="add-to-cart-js">Add To Cart</button>
+                    </div>
+                </a>
             </div>
+        `;
+    });
 
-    `
+    document.querySelector(".js-product-container").innerHTML = productHTML;
+
+    const addToCartButtons = document.querySelectorAll('.add-to-cart-js');
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent default link behavior
+            if (!isAuthenticated) {
+                window.location.href = '{% url "login" %}';
+            } else {
+                const productElement = button.closest('.product');
+                const productId = productElement.dataset.productId;
+                window.location.href = `/product/${productId}`;
+            }
+        });
+    });
 });
-
-document.querySelector(".js-product-container").innerHTML = productHTML;
-
