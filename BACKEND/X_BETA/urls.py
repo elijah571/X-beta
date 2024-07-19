9d6faf1656fb37d450f3d9600d4a544e640a8b87
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-19 17:34:02 +0100

    productlist

--- BACKEND/X_BETA/urls.py
+++ BACKEND/X_BETA/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from .views import home, signup_view, login_view, signup_success_view
+from .views import home, signup_view, login_view, signup_success_view, product_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +15,7 @@ urlpatterns = [
     path('signup_success/<int:user_id>/', signup_success_view, name='signup_success'),
     path('login/', login_view, name='login'),
     path('products/', TemplateView.as_view(template_name='products.html'), name='products'),
-    path('product/<int:id>/', TemplateView.as_view(template_name='product_detail.html'), name='product-detail'),
+    path('products/<str:product_id>/', product_detail, name='product-detail'),
     path('api/', include('products.urls')),
     path('api/', include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
