823deee380806c9ae8eb5f9255d8ff3e403147c9
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-23 11:15:47 +0100

    settings updated

--- BACKEND/X_BETA/settings.py
+++ BACKEND/X_BETA/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['.vercel.app', '.onrender.com' '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.onrender.com', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
